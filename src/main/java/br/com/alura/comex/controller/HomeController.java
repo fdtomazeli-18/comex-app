9d8e1747b43cfa7407b931ce63082a1ea8439213
fdtomazeli-18 <fdtomazeli@mppr.mp.br>
2025-09-15 14:13:05 -0300

    Add basic controller

---
+++ src/main/java/br/com/alura/comex/controller/HomeController.java
@@ -0,0 +1,18 @@
+package br.com.alura.comex.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "A aplicação está funcionando";
+    }
+
+    @GetMapping("/status")
+    public String status() {
+        return "✅ Porta 8090";
+    }
+}
